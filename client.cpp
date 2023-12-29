898dc8b2310b1d5a2a2526380d2923f8f0389470
cnzjqz-Hanz <1207452173@qq.com>
2023-12-29 12:01:37 +0800

    init

---
+++ client.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <regex>
+#include <mutex>
+#include <pthread.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/msg.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+using namespace std;
+
+int main() {
+
+}
